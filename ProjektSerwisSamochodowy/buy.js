42b405334a3801e9c6daf3b269036a35b5499d73
AlexKaminskii <115797336+AlexKaminskii@users.noreply.github.com>
2023-06-11 21:53:30 +0200

    Add files via upload

---
+++ ProjektSerwisSamochodowy/buy.js
@@ -0,0 +1,10 @@
+const akcesoria = {
+    0: ['Opony zimowe', 2000],
+    1: ['Dodatkowa gwarancja - 1 rok', 5000],
+    2: ['Dodatkowa gwarancja - 2 lata', 12000]
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+alert(document.referrer);
+});
